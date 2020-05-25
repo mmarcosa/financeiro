565a3238c814165ce7dce69250b0caaec75d1ab6
mmarcosa <marcos.barbosa8@fatec.sp.gov.br>
2020-05-24 21:43:31 -0300

    primeiro commit com a criacao do projeto

---
+++ src/main/java/com/finances/saveme/SavemeApplication.java
@@ -0,0 +1,13 @@
+package com.finances.saveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finances/saveme/SavemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finances.saveme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
